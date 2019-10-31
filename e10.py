b89212f9d696c4f736eebeafefa8bc88e4002cac
Lab6 <muhammad_zulfadhli30@connect.ite.edu.sg>
2019-10-31 11:14:01 +0800

    1.0

---
+++ e10.py
@@ -0,0 +1,21 @@
+class Pet:
+   def __init__(self,fur,breed,call):
+       self.f = fur
+       self.b = breed
+       self.c = call
+   def eat(self):
+      if self.c == 'meow':
+         print("The pet is eating fish,"
+              ,self.c)
+      elif self.c == "woof":
+          print("The pet is chewing bone,",self.c)
+class Cat(Pet):
+   def __init__(self,fur,breed):
+      Pet.__init__(self,fur,breed,'meow')
+class Dog(Pet):
+   def __init__(self,fur,breed,):
+       Pet.__init__(self,fur,breed,'woof')
+myKitty = Cat('White','Siam')
+myKitty.eat()
+myLassie = Dog('White','Collar')
+myLassie.eat()
